f3e2270d3160348073efa29e6e027c40d9b7563a
Mohsin Pannu <mohsin.pannu170@gamil.com>
2024-02-19 21:55:13 +0500

    Basic router setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
-  return <div>Hello from react app.</div>
+  return (
+    <>
+      <Header />
+
+      <Outlet />
+
+      <Footer />
+    </>
+  )
 }
 
 export default App
